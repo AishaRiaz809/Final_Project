13aef86340a22fe3d71ac8882f564f154d7dfe7d
aishariaz809 <ec23255@qmul.ac.uk>
2025-01-08 11:43:22 +0000

    unittests - loading in both datasets

--- Project_Code.py
+++ Project_Code.py
@@ -6,9 +6,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Function for importing data
+# Function for importing first dataset
 
 def data_load(file_path):
+    return pd.read_csv(file_path)
+
+# Function for importing second dataset
+
+def data_load2(file_path):
     return pd.read_csv(file_path)
 
 # Loading in first dataset
@@ -17,3 +22,8 @@ file_path = "Data/Spending_TestScores.csv"
 education_data = data_load(file_path)
 print(education_data.head())
 
+# Loading in second dataset
+
+file_path = "Data/Education_Spending.csv"
+gdp_data = data_load2(file_path)
+print(gdp_data.head())
--- Project_unittest.py
+++ Project_unittest.py
@@ -1,18 +1,27 @@
 import unittest
 import pandas as pd
 from Project_Code import data_load
+from Project_Code import data_load2
 
 class TestDataLoad(unittest.TestCase):
 
+    # Test to check first dataset is loaded in correctly
     def test_file_load(self):
         file_path = "Data/Spending_TestScores.csv"
         data = data_load(file_path)
         self.assertIsInstance(data, pd.DataFrame)
 
+    # Test to check that first datset has the correct number of columns - 6
     def test_data_columns(self):
         file_path = "Data/Spending_TestScores.csv"
         data = data_load(file_path)
         self.assertEqual(len(data.columns), 6)
+
+    # Test to check the second dataset is loaded in correctly
+    def test_file_load2(self):
+        file_path2 = "Data/Education_Spending.csv"
+        data2 = data_load2(file_path2)
+        self.assertIsInstance(data2, pd.DataFrame)
 
 if __name__ == '__main__':
     unittest.main()
