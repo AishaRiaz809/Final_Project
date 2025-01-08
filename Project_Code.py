e80a9b2a36a05db9a8c70d6bd19cf2753592d33b
aishariaz809 <ec23255@qmul.ac.uk>
2025-01-08 11:57:06 +0000

    merging first and second dataset

--- Project_Code.py
+++ Project_Code.py
@@ -27,3 +27,15 @@ print(education_data.head())
 file_path = "Data/Education_Spending.csv"
 gdp_data = data_load2(file_path)
 print(gdp_data.head())
+
+# Merging 'Public spending on education as a share of GDP' from Education_Spending into Spending_TestScores
+
+merged_data = pd.merge(
+    education_data,
+    gdp_data[['Entity', 'Year', 'Public spending on education as a share of GDP']], # columns I want to keep
+    on = ['Entity', 'Year'], # Same column names from the first and second dataset
+    how = 'left' # Merged column will be added to the left side of the dataset
+)
+
+print(merged_data.head())
+
