065a01c0ab1abc6218932b851f918a034b5460de
aishariaz809 <ec23255@qmul.ac.uk>
2025-01-08 11:26:34 +0000

    data load and unittest for second dataset

--- Project_Code.py
+++ Project_Code.py
@@ -13,14 +13,7 @@ def data_load(file_path):
 
 # Loading in first dataset
 
-file_path = "Data/Education_Spending.csv" 
+file_path = "Data/Spending_TestScores.csv" 
 education_data = data_load(file_path)
 print(education_data.head())
-
-# Loading in second dataset
-
-file_path = "Data/Spending_TestScore.csv"
-testscore_data = data_load(file_path)
-print(testscore_data.head())
-
 
--- Project_unittest.py
+++ Project_unittest.py
@@ -5,22 +5,12 @@ from Project_Code import data_load
 class TestDataLoad(unittest.TestCase):
 
     def test_file_load(self):
-        file_path = "Data/Education_Spending.csv"
+        file_path = "Data/Spending_TestScores.csv"
         data = data_load(file_path)
         self.assertIsInstance(data, pd.DataFrame)
 
     def test_data_columns(self):
-        file_path = "Data/Education_Spending.csv"
-        data = data_load(file_path)
-        self.assertEqual(len(data.columns), 4)
-
-    def test_file_load2(self):
-        file_path = "Data/Spending_TestScore.csv"
-        data = data_load(file_path)
-        self.assertIsInstance(data, pd.DataFrame)
-
-    def test_data_columns(self):
-        file_path = "Data/Spending_TestScore.csv"
+        file_path = "Data/Spending_TestScores.csv"
         data = data_load(file_path)
         self.assertEqual(len(data.columns), 6)
 
